65c8750af6913e7c1efac3328028e847de651a3e
yasincakirgithub <yasincakir.py@gmail.com>
2024-02-18 05:18:24 +0300

    update tkinter component

--- create.py
+++ create.py
@@ -26,15 +26,21 @@ def get_response_json(response):
 def get_new_question():
     sonucLabel.config(text=f"")
     while True:
- 
+        
+        subject = subjecttextarea.get("1.0",tk.END).strip()
+  
+        if subject == "" or subject is None:
+            subject = "Genel kültür coğrafya veya tarih"
+        
+        print("Subject",subject,type(subject))
         response = g4f.ChatCompletion.create(
             model=g4f.models.gpt_35_turbo_16k_0613,
             
             messages=[{
                 "role": "user",
                 "content": """
-                    KPSS lisans genel kültür coğrafya veya tarih sorusu oluşturur musun?
-                    Sorunun içeriği 19.yüzyıl Osmanlı Devleti Islahatlarla ilgili sorular olsun.
+                    KPSS lisans sınavı için """+ subject +""" konusunda sorusu oluşturmanı istiyorum.
+                    Kısa uzunlukta soru olmasın.
                     5 şıklı,şıklar A,B,C,D ve E olacak.
                     Sonucu question,choices ve answer keyleri içerek json olarak yazdır.
                     Örneğin { "question": "example question","choices":{"A": "cevap1","B": "cevap2","C": "cevap3","D": "cevap4","E": "cevap5"} , "answer":"C" } gibi. 
@@ -54,11 +60,11 @@ def get_new_question():
 
             question_answer.set(answer)
 
-            secenek1.config(text=choices.get("A"))
-            secenek2.config(text=choices.get("B"))
-            secenek3.config(text=choices.get("C"))
-            secenek4.config(text=choices.get("D"))
-            secenek5.config(text=choices.get("E"))
+            secenek1.config(text="A) " + choices.get("A"))
+            secenek2.config(text="B) " + choices.get("B"))
+            secenek3.config(text="C) " + choices.get("C"))
+            secenek4.config(text="D) " + choices.get("D"))
+            secenek5.config(text="E) " + choices.get("E"))
 
             break
 
@@ -74,46 +80,60 @@ def check_answer_is_true():
 
 
 pencere = tk.Tk()
+pencere.config(padx=40,pady=20)
 pencere.geometry("1000x800")
 pencere.title("KPSS SORU OLUŞTURUCU")
 
 question_answer = tk.StringVar()
+selected_answer = tk.StringVar()
+subject = tk.StringVar()
+
+
+#KONU METNİ
+subject_panel = tk.Frame(pencere)
+subject_panel.grid(row=1, column=1)
+
+subjectLabel = tk.Label(subject_panel,text="Konu")
+subjectLabel.grid(row=1, column=1)
+
+subjecttextarea = tk.Text(subject_panel, height=1, width=40,font=font.Font(size=15),padx=10,pady=10)
+subjecttextarea.grid(row=1, column=2)
 
 
 #SORUNUN METNİ
+question_panel = tk.Frame(pencere,bg="green")
+question_panel.grid(row=2, column=1)
 
-sorutag = tk.Label(pencere, text="Soru:")
-sorutag.grid(row=1, column=1, padx=40)
-sorutextarea = tk.Text(pencere, height=20, width=50,padx=40,pady=20,font=font.Font(family="Arial", size=15, weight="bold"))
-sorutextarea.grid(row=2, column=1, padx=40)
+sorutextarea = tk.Text(question_panel, height=10, width=50,font=font.Font(size=15),padx=10,pady=10)
+sorutextarea.grid(row=1, column=1)
 
 
 # SEÇENEKLERİN GETİRİLMESİ
-panel = tk.Frame(pencere, height=20, width=50, padx=40)
-panel.grid(row=3, column=1, padx=40)
+answer_panel = tk.Frame(pencere)
+answer_panel.grid(row=3, column=1,pady=20)
 
-selected_answer = tk.StringVar()
-secenek1=tk.Radiobutton(panel, text="A", variable=selected_answer, value="A",wraplength=100,font=font.Font(family="Arial", size=13, weight="bold"))
-secenek1.grid(row=1, column=1, sticky="w")
-secenek2=tk.Radiobutton(panel, text="B", variable=selected_answer, value="B",wraplength=100,font=font.Font(family="Arial", size=13, weight="bold"))
-secenek2.grid(row=1, column=2, sticky="w")
-secenek3=tk.Radiobutton(panel, text="C", variable=selected_answer, value="C",wraplength=100,font=font.Font(family="Arial", size=13, weight="bold"))
-secenek3.grid(row=1, column=3, sticky="w")
-secenek4=tk.Radiobutton(panel, text="D", variable=selected_answer, value="D",wraplength=100,font=font.Font(family="Arial", size=13, weight="bold"))
-secenek4.grid(row=1, column=4, sticky="w")
-secenek5=tk.Radiobutton(panel, text="E", variable=selected_answer, value="E",wraplength=100,font=font.Font(family="Arial", size=13, weight="bold"))
-secenek5.grid(row=1, column=5, sticky="w")
+secenek1=tk.Radiobutton(answer_panel, variable=selected_answer, value="A",wraplength=100,font=font.Font(size=10))
+secenek1.grid(row=1, column=1)
+secenek2=tk.Radiobutton(answer_panel, variable=selected_answer, value="B",wraplength=100,font=font.Font(size=10))
+secenek2.grid(row=1, column=2)
+secenek3=tk.Radiobutton(answer_panel, variable=selected_answer, value="C",wraplength=100,font=font.Font(size=10))
+secenek3.grid(row=1, column=3)
+secenek4=tk.Radiobutton(answer_panel, variable=selected_answer, value="D",wraplength=100,font=font.Font(size=10))
+secenek4.grid(row=1, column=4)
+secenek5=tk.Radiobutton(answer_panel, variable=selected_answer, value="E",wraplength=100,font=font.Font(size=10))
+secenek5.grid(row=1, column=5)
 
-sonucLabel = tk.Label(panel,text="")
-sonucLabel.grid(row=1, column=6, padx=20)
+cevabigonderbutton = tk.Button(answer_panel, text="Sonucu Göster", command=check_answer_is_true,font=font.Font(size=12))
+cevabigonderbutton.grid(row=1, column=6)
+
+sonucLabel = tk.Label(answer_panel,text="")
+sonucLabel.grid(row=1, column=7, padx=20)
 
 
 #AKSİYONLAR
-cevabigonderbutton = tk.Button(pencere, text="Sonucu Göster", command=check_answer_is_true,font=font.Font(family="Arial", size=15, weight="bold"))
-cevabigonderbutton.grid(row=8, column=1, padx=40)
-
-yenisorubutton = tk.Button(pencere, text="Yeni Soru", command=get_new_question,font=font.Font(family="Arial", size=15, weight="bold"))
-yenisorubutton.grid(row=9, column=1, padx=40)
+yenisorubutton = tk.Button(pencere, text="Yeni Soru", command=get_new_question,font=font.Font(size=12))
+yenisorubutton.grid(row=4, column=1)
 
 # Pencereyi başlat
+get_new_question()
 pencere.mainloop()
